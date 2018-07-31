a7a08756b5871d53d024a2c0f0d275f79daacac1
candaceww <candacew224@gmail.com>
2018-07-31 18:36:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,17 @@ require 'sinatra'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/'
+  get '/name' do
+    "My name is Candace"
+  end
+  
+  get '/hometown' do
+    "My hometown is Manhattan"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Nice for What"
+  end
   
   
   
